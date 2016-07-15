d34890e20fec25ea8f87d033c77f4c2aeaf0970b
Ricardo Hernan Herrera Valle <aherreric@gmail.com>
2016-07-15 15:57:33 -0500

    About ViewController

--- AllStars/AllStars/iPhone/Classes/About/AboutCollaboratorsViewController.swift
+++ AllStars/AllStars/iPhone/Classes/About/AboutCollaboratorsViewController.swift
@@ -8,9 +8,50 @@
 
 import Foundation
 
-class  AboutCollaboratorsViewController: UIViewController {
+class  AboutCollaboratorsViewController: UIViewController, UICollectionViewDataSource {
     
     
+    @IBOutlet weak var collaboratorsCollectionView: UICollectionView!
     
+    lazy var collaborators: [NSDictionary] = {
+        
+        let collaboratorsPath: NSURL! = NSBundle.mainBundle().URLForResource("Collaborators", withExtension: "plist")
+        
+        let collaboratorsPlist: NSDictionary! = NSDictionary(contentsOfURL: collaboratorsPath)
+        
+        let array = collaboratorsPlist["Collaborators"] as! [NSDictionary]
+        
+        return array
+    }()
     
+    // MARK: - Initialization
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return collaborators.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let collaborator = collaborators[indexPath.row]
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollaboratorCell", forIndexPath: indexPath) as! CollaboratorCell
+        
+        cell.userName.text = collaborator["Name"] as? String
+        cell.userImage.image = UIImage(named: collaborator["Pic"] as! String)
+        
+        return cell
+    }
+}
+
+class CollaboratorCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
 }
--- AllStars/AllStars/iPhone/Classes/About/AboutViewController.swift
+++ AllStars/AllStars/iPhone/Classes/About/AboutViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import Foundation
+import MessageUI
 
-
-class AboutViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+class AboutViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, MFMailComposeViewControllerDelegate {
     
+    // MARK: - Main view variables
     var pageViewController = UIPageViewController()
     
     @IBOutlet weak var pageControl: UIPageControl!
     
+    // MARK: - Info Texts variables
+    
+    @IBOutlet weak var whatLabel: UILabel!
+    @IBOutlet weak var whatLabelText: UILabel!
+    @IBOutlet weak var whyLabel: UILabel!
+    @IBOutlet weak var whyLabelText: UILabel!
+    @IBOutlet weak var whoLabel: UILabel!
+    @IBOutlet weak var whoLabelText: UILabel!
+    
+    // MARK: - Lincense variables
+    @IBOutlet weak var licenseTextView: UITextView!
     
     lazy var arrayViewControllers : [UIViewController] = {
         
@@ -22,7 +34,9 @@ class AboutViewController: UIViewController, UIPageViewControllerDataSource, UIP
         
         let collaboratorsAbout = self.storyboard?.instantiateViewControllerWithIdentifier("CollaboratorsViewController")
         
-        return [controllerTextInfo!, collaboratorsAbout!]
+        let licenseAbout = self.storyboard?.instantiateViewControllerWithIdentifier("LicenseViewController")
+        
+        return [controllerTextInfo!, collaboratorsAbout!, licenseAbout!]
     }()
     
     // MARK: - Initialization
@@ -30,13 +44,48 @@ class AboutViewController: UIViewController, UIPageViewControllerDataSource, UIP
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard let pageControl = self.pageControl
+            else {                
+                settexts()
+                return
+        }
+        
         pageControl.numberOfPages = arrayViewControllers.count
-//        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "ic_nav_header.jpg")!)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
     // MARK: - Style
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
+    }
+    
+    func settexts() {
+        
+        setInfoTexts()
+        setLicenseText()
+    }
+    
+    func setInfoTexts() {
+        
+        guard let _ = self.whatLabel
+            else { return }
+        
+        whatLabel.text = "what".localized
+        whatLabelText.text = "about_what".localized
+        whyLabel.text = "why".localized
+        whyLabelText.text = "about_why".localized
+        whoLabel.text = "who".localized
+        whoLabelText.text = "about_who".localized
+    }
+    
+    func setLicenseText(){
+        
+        guard let license = self.licenseTextView
+            else { return }
+        
+        license.text = "about_license".localized
+        license.textColor = UIColor.whiteColor()
     }
     
     // MARK: - UIPageViewControllerDataSource
@@ -103,6 +152,39 @@ class AboutViewController: UIViewController, UIPageViewControllerDataSource, UIP
         let currentIndex = arrayViewControllers.indexOf((self.pageViewController.viewControllers?.first)!)
         
         self.pageViewController.setViewControllers([initialController], direction: currentIndex < newIndex ? UIPageViewControllerNavigationDirection.Forward : UIPageViewControllerNavigationDirection.Reverse, animated: true, completion: nil)
+    }
+    
+    @IBAction func sendMessage(sender: AnyObject) {
+        
+        self.sendEmail()
+    }
+    
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["email_support".localized])
+            mail.setSubject("email_subject".localized)
+            
+            presentViewController(mail, animated: true, completion: nil)
+        } else {
+            // show failure alert
+            
+            let alert: UIAlertController = UIAlertController(title: "email_error_message".localized, message: nil, preferredStyle: .ActionSheet)
+            
+            
+            let cancelAction = UIAlertAction(title: "ok".localized, style: .Cancel,
+                                             handler: nil)
+
+            alert.addAction(cancelAction)
+            
+            self.presentViewController(alert, animated: true, completion: {})
+        }
+    }
+    
+    // MARK: - MFMailComposeViewControllerDelegate
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
     }
     
     // MARK: - Navigation
