a14a9d40421ecbfa1223f5b033ea71d0b7fe2f13
Erik Flores <erikfloresq@gmail.com>
2017-06-13 16:43:44 -0500

    Fix moreNavigationbar edit button

--- Bx Connect/MainTabBarController.swift
+++ Bx Connect/MainTabBarController.swift
@@ -16,28 +16,14 @@ class MainTabBarController: UITabBarController {
         self.moreNavigationController.topViewController?.view.tintColor = #colorLiteral(red: 0.9450471997, green: 0.3605467975, blue: 0.04605709761, alpha: 1)
         self.moreNavigationController.navigationBar.barTintColor = #colorLiteral(red: 0.9450471997, green: 0.3605467975, blue: 0.04605709761, alpha: 1)
         self.moreNavigationController.navigationBar.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        //self.moreNavigationController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)]
         self.moreNavigationController.navigationBar.barStyle = .black
-        
-        if #available(iOS 11.0, *) {
-            self.moreNavigationController.navigationBar.prefersLargeTitles = true
-        } else {
-            // Fallback on earlier versions
-        }
+        self.customizableViewControllers = nil
         
     }
     
-    /*
-     UIView*          edit_views = [tabBarController.view.subviews objectAtIndex:1];
-     UINavigationBar* bar        = [[edit_views subviews]objectAtIndex:1];
-     
-     bar.barTintColor = [UIColor redColor];
-     bar.tintColor    = [UIColor whiteColor];
-     for (int i = 3; i < [edit_views.subviews count]; i++)
-     {
-     UIButton *button = [[edit_views subviews]objectAtIndex:i];
-     button.tintColor = [UIColor redColor];
-     }
-     */
+    override func viewDidAppear(_ animated: Bool) {
+        self.moreNavigationController.navigationBar.topItem?.rightBarButtonItem = nil
+    }
+ 
 
 }
